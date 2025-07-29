bc8a394ef7608ccf10b73a1a049b518fd931b975
KandulaRamya15 <kandularamyachowdary@gmail.com>
2025-07-29 11:17:06 +0530

    my project notes

---
+++ react-workspace/frontend/src/components/FooterComponent.js
@@ -0,0 +1,9 @@
+function FooterComponent()
+{
+    return(
+        <footer className="footer">
+               <small>All Right Reserved &copy; 2025 by RamyaChowdary</small> 
+        </footer>
+    )
+}
+export default FooterComponent;
